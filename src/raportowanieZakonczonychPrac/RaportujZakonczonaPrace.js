b833644eae2b29f2626d8d6ef70dd4de14baf371
ar-insoft <arkadiusz.sienkiewicz@invsoft.pl>
2021-01-14 13:54:12 +0100

    first commit

---
+++ src/raportowanieZakonczonychPrac/RaportujZakonczonaPrace.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import preval from 'preval.macro'
+
+export const RaportujZakonczonaPrace = () => {
+
+    return (
+        <div className="mainPanel">
+            <div>
+                RaportujZakonczonaPrace
+            </div>
+        </div>
+    )
+}
