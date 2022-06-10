25c8baf32fb679c4ba20ec1988142c619f8e35ff
ar-insoft <arkadiusz.sienkiewicz@invsoft.pl>
2022-06-10 10:35:51 +0200

    aktualizacja bibliotek m.in. react 18.1

--- src/raportowanieZakonczonychPrac/PanelSemantic.js
+++ src/raportowanieZakonczonychPrac/PanelSemantic.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Form, Button, Table, Container, Segment } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
 import './PanelSemantic.css'
 import classNames from 'classnames/bind'
 import _ from 'lodash'
